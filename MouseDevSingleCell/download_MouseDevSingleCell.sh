4c40dd7dcebf9c17d04ccf94db618cf533faff9d
badoi <badoi@me.com>
2020-05-07 16:11:38 -0400

    update mouse dev scripts

--- MouseDevSingleCell/download_MouseDevSingleCell.sh
+++
@@ -1,2 +0,0 @@
-wget https://github.com/jeremymsimon/MouseCortex/raw/master/E14_combined_matrix.txt.gz
-wget https://github.com/jeremymsimon/MouseCortex/raw/master/P0_combined_matrix.txt.gz
---
+++ MouseDevSingleCell/step0_download_MouseDevSingleCell.sh
@@ -0,0 +1,4 @@
+wget https://github.com/jeremymsimon/MouseCortex/raw/master/E14_combined_matrix.txt.gz
+wget https://github.com/jeremymsimon/MouseCortex/raw/master/P0_combined_matrix.txt.gz
+wget https://github.com/jeremymsimon/MouseCortex/blob/master/P0_combined_matrix_ClusterAnnotations.txt
+wget https://github.com/jeremymsimon/MouseCortex/blob/master/E14_combined_matrix_ClusterAnnotations.txt
