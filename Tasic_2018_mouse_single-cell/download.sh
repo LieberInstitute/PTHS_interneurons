bc1025abf9f8f5404bd4f1d8365aa13432815a3d
badoi <badoi@me.com>
2022-01-05 15:25:16 -0500

    script to download allen mouse snRNA data

---
+++ Tasic_2018_mouse_single-cell/download.sh
@@ -0,0 +1,7 @@
+wget https://transcriptomic-viewer-downloads.s3-us-west-2.amazonaws.com/mouse/transcriptome.zip
+wget https://transcriptomic-viewer-downloads.s3-us-west-2.amazonaws.com/mouse/dendrogram.zip
+wget https://transcriptomic-viewer-downloads.s3-us-west-2.amazonaws.com/mouse/sample-cluster-probabilities.zip
+wget https://transcriptomic-viewer-downloads.s3-us-west-2.amazonaws.com/mouse/taxonomy.zip
+wget https://transcriptomic-viewer-downloads.s3-us-west-2.amazonaws.com/mouse/sample-annotations.zip
+wget https://brainmapportal-live-4cc80a57cd6e400d854-f7fdcae.divio-media.net/filer_public/49/6e/496ea0c2-25ae-4523-bb84-8d51a63ab72e/readme_mouse.txt
+
